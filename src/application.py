38c5f4f63e7f6093643fdc7038afdd9dbf55bca9
Shutang Wang <sw3532@columbia.edu>
2022-10-27 23:07:36 -0400

    change appointment_time to list, support raw json

--- src/application.py
+++ src/application.py
@@ -16,7 +16,6 @@ CORS(app)
 @app.route('/', methods=["GET"])
 def index():
     return 'You have reached the index page!'
-
 @app.get("/api/health")
 def get_health():
     t = str(datetime.now())
@@ -69,15 +68,19 @@ def get_courses_by_userid(user_id):
 
 @app.route("/course/create", methods=["POST"])
 def create_course():
-    json_dict = request.form
+    json_dict = request.get_json()
     user_id = str(json_dict["user_id"])
     teacher_id = str(json_dict["teacher_id"])
     create_time = json_dict["create_time"] #yyyy-mm-dd HH:mm:ss
     appointment_time = json_dict["appointment_time"] #yyyy-mm-dd HH:mm:ss
     price = float(json_dict["price"])
 
-    fields = (user_id, teacher_id, create_time, appointment_time, price)
-    res = CoursesResource.create_course_by_field(fields)
+    # fields = (user_id, teacher_id, create_time, appointment_time, price)
+    # res = CoursesResource.create_course_by_field(fields)
+    res = []
+    for time in appointment_time:
+        fields = (user_id, teacher_id, create_time, time, price)
+        res.append(CoursesResource.create_course_by_field(fields))
 
     if res:
         rsp = Response(json.dumps(res, default=str), status=200, content_type="application.json")
